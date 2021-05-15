35b1aba37aa257db2726ca3cd062bce1d7e5ac34
Fabricio <fabemanrique@hotmail.com>
2021-05-15 18:49:59 -0300

    I have created the player movement script and tune the movement values

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+#pragma warning disable 0649
     [SerializeField] private Transform PlayerLookCameraTransform;
-    [SerializeField] private float HorizontalLookSensitivity = 100.0f;
+    [SerializeField] private float HorizontalLookSensitivity = 120.0f;
     [SerializeField] private float VerticalLookSensitivity = 100.0f;
     [SerializeField] private float TopMaxVerticalLookAngles = 90.0f;
-    [SerializeField] private float BottomMinVerticalLookAngles = -70.0f;
+    [SerializeField] private float BottomMinVerticalLookAngles = -80.0f;
     [SerializeField] private bool InvertVerticalLook = false;
     
     [SerializeField] private string HorizontalAxisName = "Mouse X";
     [SerializeField] private string VerticalAxisName = "Mouse Y";
-    
+#pragma warning restore 0649
+
     private float currentFrameHorizontalAxisValue = 0;
     private float currentFrameVerticalAxisValue = 0;
     private Vector3 verticalRotation = Vector3.zero;
@@ -27,19 +26,22 @@ public class PlayerLook : MonoBehaviour
         localCachedTransform = this.GetComponent<Transform>();
     }
 
-    void Update()
+    private void Update()
     {
         currentFrameHorizontalAxisValue = Input.GetAxis(HorizontalAxisName) * HorizontalLookSensitivity * Time.deltaTime;
         currentFrameVerticalAxisValue = Input.GetAxis(VerticalAxisName) * VerticalLookSensitivity * Time.deltaTime;
 
-        if (InvertVerticalLook)
-            verticalRotation.x += currentFrameVerticalAxisValue;
-        else
-            verticalRotation.x -= currentFrameVerticalAxisValue;
+        AdjustVerticalAxisValueIfInvertedAxis();
         
+        verticalRotation.x -= currentFrameVerticalAxisValue;
         verticalRotation.x = Mathf.Clamp(verticalRotation.x, BottomMinVerticalLookAngles, TopMaxVerticalLookAngles);
         
         PlayerLookCameraTransform.localRotation = Quaternion.Euler(verticalRotation);
         localCachedTransform.Rotate(Vector3.up * currentFrameHorizontalAxisValue);
+    }
+
+    private void AdjustVerticalAxisValueIfInvertedAxis()
+    {
+        if (InvertVerticalLook) currentFrameVerticalAxisValue *= -1;
     }
 }
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] private float HorizontalSpeed = 5.0f;
+    [SerializeField] private float VerticalSpeed = 6.0f;
+    
+    [SerializeField] private string HorizontalAxisName = "Horizontal";
+    [SerializeField] private string VerticalAxisName = "Vertical";
+#pragma warning restore 0649
+
+    private float currentFrameHorizontalAxisValue = 0;
+    private float currentFrameVerticalAxisValue = 0;
+    
+    private Vector3 movePosition = Vector3.zero;
+
+    private CharacterController cachedLocalCharacterController;
+
+    private void Awake()
+    {
+        cachedLocalCharacterController = this.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        currentFrameHorizontalAxisValue = Input.GetAxis(HorizontalAxisName) * Time.deltaTime * HorizontalSpeed;
+        currentFrameVerticalAxisValue = Input.GetAxis(VerticalAxisName) * Time.deltaTime * VerticalSpeed;
+
+        movePosition = (transform.right * currentFrameHorizontalAxisValue) + (transform.forward * currentFrameVerticalAxisValue);
+
+        cachedLocalCharacterController.Move(movePosition);
+    }
+}
